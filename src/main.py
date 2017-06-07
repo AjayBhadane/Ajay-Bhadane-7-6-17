34d2bb2d4457da932b04e0e8bb2fd7cf40de2a56
AjayBhadane <ajaybhadane95@gmail.com>
2017-06-07 09:01:58 -0700

    Cache implementation

--- src/main.py
+++ src/main.py
@@ -1,17 +1,40 @@
+from xml.etree import ElementTree
+
 class Cache:
 
     max_capacity = 20
 
     def __init__(self):
-        self.data_element = {
-            'id': 1,
-            'name': "Ajay Bhadane",
-            'math_marks': 100,
-            'science_marks': 100,
-        }
+        all_students = ElementTree.parse('data.xml').findall('student')
+        self.data = []
+        for student in all_students:
+            data_element = {
+                'id': int(student.attrib['id']),
+                'name': student.attrib['name'],
+                'math_marks': int(student.attrib['math_marks']),
+                'science_marks': int(student.attrib['science_marks']),
+            }
+            self.data.append(data_element)
 
     def run(self):
-        print(self.data_element['name'])
+        keep_running = True
+        while keep_running:
+            choice = input("Enter your choice: ")
+
+            if choice == 'Add':
+                self.add_element()
+            elif choice == 'Remove':
+                self.remove_element()
+            elif choice == 'Get':
+                self.return_element()
+            elif choice == 'CRUD':
+                self.CRUD()
+            elif choice == 'Help':
+                self.display_help()
+            elif choice == 'Exit':
+                keep_running = False
+            else:
+                print("Invalid choice!")
 
 
 def main():
