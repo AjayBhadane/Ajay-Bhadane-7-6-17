003b1da54d1d0260dbdaf60eeef478796f325a5a
AjayBhadane <ajaybhadane95@gmail.com>
2017-06-07 10:46:01 -0700

    Cache implementation

--- src/main.py
+++ src/main.py
@@ -5,8 +5,12 @@ class Cache:
     max_capacity = 20
 
     def __init__(self):
+        self.load_from_file()
+
+    def load_from_file(self):
         all_students = ElementTree.parse('data.xml').findall('student')
         self.data = []
+
         for student in all_students:
             data_element = {
                 'id': int(student.attrib['id']),
@@ -19,23 +23,38 @@ class Cache:
     def run(self):
         keep_running = True
         while keep_running:
-            choice = input("Enter your choice: ")
+            choice = input('\nOptions are "Create", "Read", "Update", "Delete", "Exit" \nEnter your choice:')
 
-            if choice == 'Add':
+            if choice == 'Create':
                 self.add_element()
-            elif choice == 'Remove':
+            elif choice == 'Read':
+                self.read_element()
+            elif choice == 'Update':
+                self.update_element()
+            elif choice == 'Delete':
                 self.remove_element()
-            elif choice == 'Get':
-                self.return_element()
-            elif choice == 'CRUD':
-                self.CRUD()
-            elif choice == 'Help':
-                self.display_help()
             elif choice == 'Exit':
+                self.upload_to_file()
                 keep_running = False
             else:
                 print("Invalid choice!")
 
+    def add_element(self):
+
+        self.data.append({
+            'id': int(input("Enter id:")),
+            'name': input("Enter name:"),
+            'math_marks': int(input("Enter math marks:")),
+            'science_marks': int(input("Enter science marks")),
+        })
+
+    def upload_to_file(self):
+        root = ElementTree.Element('students')
+
+        for student in self.data:
+            ElementTree.SubElement(root, "student", id=str(student['id']), name=student['name'],math_marks=str(student['math_marks']),science_marks=str(student['science_marks']))
+
+        ElementTree.ElementTree(root).write("data.xml")
 
 def main():
 
