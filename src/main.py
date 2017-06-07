52a56baad12040e942bef100d7136910d42d3cf8
AjayBhadane <ajaybhadane95@gmail.com>
2017-06-07 08:40:13 -0700

    Cache implementation

--- src/main.py
+++ src/main.py
@@ -1,18 +1,24 @@
+class Cache:
+
+    max_capacity = 20
+
+    def __init__(self):
+        self.data_element = {
+            'id': 1,
+            'name': "Ajay Bhadane",
+            'math_marks': 100,
+            'science_marks': 100,
+        }
+
+    def run(self):
+        print(self.data_element['name'])
+
+
 def main():
+
     cache = Cache()
     cache.run()
 
 
 if __name__ == "__main__":
     main()
-
-
-class Cache:
-
-    max_capacity = 20
-
-    def __init__(self):
-        pass
-
-    def run(self):
-        pass
